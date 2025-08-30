196d392cdced20451c3abba3fe3f12e86e02bfb9
mlee11-m <mlee0131@student.monash.edu>
2025-08-31 00:12:31 +1000

    added bootstrap to all files, now website looks better

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true })); // <-- put this BEFORE json
 app.use(bodyParser.json());
 app.use(express.static("public")); // serve static files
-
+app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
 // View engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
